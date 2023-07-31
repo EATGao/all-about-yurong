1c44de0d703c57268628709697a23aa25722f53f
EATGao <gaoyurong923@gmail.com>
2023-08-01 08:23:44 +1000

    change sidebar location

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Header from './Header/Header';
 import Home from './Pages/Home/Home';
-import Sidebar from './Sidebar';
+import Sidebar from './Sidebar/Sidebar';
 
 function App() {
   return (
