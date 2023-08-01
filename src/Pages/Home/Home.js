fc6ea42eca14b9ea3abbca935435ce371de9b7ec
EATGao <gaoyurong923@gmail.com>
2023-08-01 10:56:25 +1000

    Home page version 1 finished

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,7 +3,27 @@ import './Home.css'
 
 function MainPage() {
   return (
-    <div>MainPage</div>
+    <div className='home'>
+      <div className='home__top'>
+        <div class="flip__card">
+          <div class="flip__card__inner">
+            <div class="flip__card__front">
+              <img src="https://theeventchronicle.com/wp-content/uploads/2021/04/mobile-app-development-programming.jpg" alt="Avatar"/>
+            </div>
+            <div class="flip__card__back">
+              <h1>John Doe</h1>
+              <p>Architect & Engineer</p>
+              <p>We love that guy</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div className='home_bottom'>
+
+      </div>
+    </div>
   )
 }
 
