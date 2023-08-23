31c352ed14a48ee2b273c8a7863fe99637b208b7
EATGao <gaoyurong923@gmail.com>
2023-08-23 15:15:32 +1000

    add demo to daily reader page

--- src/Pages/ProjectDetail/DailyReader.js
+++ src/Pages/ProjectDetail/DailyReader.js
@@ -3,9 +3,11 @@ import Header from '../../Header/Header';
 import Footer from '../../Footer/Footer';
 import './DailyReader.css';
 import SecondHeader from '../../Header/SecondHeader';
+import DailyReaderDemo from '../../Pictures/DailyReader/dailyreader_demo.mp4'
 
 function DailyReader() {
-  const intro = (
+  const aims = (<>1. Help users to build daily reading behaviors.{'\n'} 2. build a comfortable reading atmosphere for users{'\n'}</>)
+  const functionalities = (
     <>This is an Android application made in Java. It is a teamwork project and its core functionalities are:{'\n'}
     - Using Firebase Authentication to achieve login and signup functions.{'\n'}
     - Applying LiveData to reflect the data change immediately.{'\n'}
@@ -21,15 +23,24 @@ function DailyReader() {
         <div className='dailyreader__left'>
           <h1>Daily Reader</h1>
           <div className='dailyreader__demo'>
-            Demo video
+            <video autoPlay muted loop>
+              <source type='video/mp4' src={DailyReaderDemo}></source>
+            </video>
           </div>
         </div>
         <div className='dailyreader__right'>
           <div className='dailyreader__introduction'>
-            {intro}
-          </div>
-          <div className='dailyreader__tech'>
-            <table>
+            <div className='dailyreader__aims'>
+              <h2>Why we built this?</h2>
+              {aims}
+            </div>
+            <div className='dailyreader__funcs'>
+              <h2>Functionalities</h2>
+              {functionalities}
+            </div>
+            <div className='dailyreader__tech'>
+              <h2>Tech used</h2>
+              <table>
                 <tbody>
                   <tr>
                     <th>Platform</th>
@@ -50,13 +61,18 @@ function DailyReader() {
                 </tbody>
               </table>
             </div>
-            <div className='dailyreader__access'>
-              <button><a href='https://github.com/Michael-Vio/DailyReader' target='_blank' rel="noreferrer">Source code</a></button>
+            <div className='dailyreader__role'>
+              <h2>My role</h2>
+              Team Leader & Developer & Icon designer
             </div>
           </div>
+          <div className='dailyreader__access'>
+            <button><a href='https://github.com/Michael-Vio/DailyReader' target='_blank' rel="noreferrer">Source code</a></button>
+          </div>
         </div>
-      <Footer/>
-    </div>
+      </div>
+    <Footer/>
+  </div>
   )
 }
 
