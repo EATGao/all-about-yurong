461de3e0bd2532f10c5ddec0fca954e5f0b654fb
EATGao <gaoyurong923@gmail.com>
2024-01-03 17:51:13 +1100

    refactor: change email copied message to material UI component to improve UX

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import './Home.css';
-import SocialMedia from './SocialMedia'
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import AttachEmailIcon from '@mui/icons-material/AttachEmail';
 import Flame from './Flame';
 import ContentMinHeight from '../../Util/ContentMinHeight'
+import { Alert } from '@mui/material'
+import { useState } from 'react';
 
 function MainPage() {
-  
+  const [alert, setAlert] = useState(false);
+  const [alertType, setAlertType] = useState('success');
+  const [alertContent, setAlertContent] = useState('');
+
+  const handleClick = (isWeb, link) => {
+    if (isWeb) {
+      window.open(link, '_blank')
+    } else {
+      navigator.clipboard.writeText(link)
+      .then(() => {
+        setAlertContent('Email address has copied to your clipboard!');
+        setAlert(true)
+      })
+      .catch((error) => {
+        setAlertType('error');
+        setAlertContent('Copy Email address to clipboard failed. Please try again.');
+        setAlert(true)
+      });
+    }
+    // console.log(isWeb + link)
+  }
+
   return (
     <div className='home' style={{ minHeight: ContentMinHeight() }}>
       <div className='home__left'>
@@ -23,9 +45,18 @@ function MainPage() {
           </div>
         </div>
         <div className='home__social__media'>
-          <SocialMedia IconButton={GitHubIcon} title='GitHub' isWeb={true} url='https://github.com/EATGao'/>
-          <SocialMedia IconButton={LinkedInIcon} title='Linkedin' isWeb={true} url='https://www.linkedin.com/in/yurong-gao-933b411a1/'/>
-          <SocialMedia IconButton={AttachEmailIcon} title='Email' isWeb={false} email='gaoyurong923@gmail.com'/>
+          <div className='social__media'>
+            <GitHubIcon style={{ width: 40, height: 50 }} onClick={() => {handleClick(true, 'https://github.com/EATGao')}}/>
+          </div>
+          <div className='social__media' onClick={() => {handleClick(true, 'https://www.linkedin.com/in/yurong-gao-933b411a1/')}}>
+            <LinkedInIcon style={{ width: 40, height: 50 }}/>
+          </div>
+          <div className='social__media' onClick={() => {handleClick(false, 'gaoyurong923@gmail.com')}}>
+            <AttachEmailIcon style={{ width: 40, height: 50 }}/>
+          </div>
+        </div>
+        <div className='email__copy__alert'>
+          {alert && <Alert className='alert' severity={alertType} onClose={() => {setAlert(false)}}>{alertContent}</Alert>}
         </div>
       </div>
       <div className='home__right'>
