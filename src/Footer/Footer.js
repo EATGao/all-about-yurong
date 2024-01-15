37c1e29d8b8835eb8ac2bbaa80b63bf8183cf877
EATGao <gaoyurong923@gmail.com>
2024-01-15 17:48:27 +1100

    refactor: use semantic elements in footer

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -3,14 +3,14 @@ import './Footer.css'
 
 function Footer() {
   return (
-    <div className='footer'>
+    <footer className='footer'>
         <div className='footer__left'>
             <p>@ 2023 Yurong Gao</p>
         </div>
         <div className='footer__right'>
             <p>Built with <strong>ReactJS</strong></p>
         </div>
-    </div>
+    </footer>
   )
 }
 
